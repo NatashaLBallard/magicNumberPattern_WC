542f5a1d00708e4500401f4679c0204bdac16857
NatashaLBallard <nbitcareer@gmail.com>
2018-01-26 11:20:31 -0500

    checking for even/odd works, need to add loop that stores the number and repeats until it equals 1

--- src/main/java/com/magicnumberpattern_wc/demo/MainController.java
+++ src/main/java/com/magicnumberpattern_wc/demo/MainController.java
@@ -10,15 +10,32 @@ import java.util.Scanner;
 public class MainController {
 
     @RequestMapping("/")
-    public String magicnumberpattern(@RequestParam("name") String name) {
+    public String magicnumberpattern()
+    //(@RequestParam("number") int number) {
+    {
 
-        return name;
+        int urlNum = 13;
+        int evenNum = 0;
+        int listOfNums = 0;
 
+        while (urlNum > 0) {
+            if ((Integer.valueOf(urlNum) % 2 == 0)) {
+                listOfNums = urlNum / 2;
+            } else if (Integer.valueOf(urlNum) % 2 != 0) {
+                listOfNums = (urlNum * 3) + 1;
+            }
 
+            
+            }
+            //System.out.println(number);
+            //return String.valueOf(number);
 
+            System.out.println();
+            return String.valueOf(listOfNums);
 
 
 
 
     }
+
 }
