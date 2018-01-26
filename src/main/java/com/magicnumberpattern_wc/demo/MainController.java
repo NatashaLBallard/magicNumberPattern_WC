219f87c8c8d8e4d5d97950179c185913f1d12f4a
NatashaLBallard <nbitcareer@gmail.com>
2018-01-26 15:51:59 -0500

    Required paramater with @RequestParam

--- src/main/java/com/magicnumberpattern_wc/demo/MainController.java
+++ src/main/java/com/magicnumberpattern_wc/demo/MainController.java
@@ -10,37 +10,37 @@ import java.util.Scanner;
 public class MainController {
 
     @RequestMapping("/")
-    public String magicnumberpattern()
-    //(@RequestParam("number") int number) {
-    {
+    public String magicnumberpattern(@RequestParam("number") String number) {
+        {
 
-        int urlNum = 21;
-        int storedNum = 0;
-        String listOfNums = "";
-
-        do {
-
-            if ((urlNum % 2 == 0)) {
-                urlNum = urlNum / 2;
-                listOfNums += urlNum + " ";
-            } else if (urlNum % 2 == 1) {
-                urlNum = (urlNum * 3) + 1;
-                listOfNums += urlNum + " ";
+            if (number == null){
+                return "Enter a valid number.";
             }
+            int urlNum = Integer.parseInt(number);
+            String listOfNums = "";
 
-            //System.out.println(number);
-            //return String.valueOf(number);
+            do {
+
+                if ((urlNum % 2 == 0)) {
+                    urlNum = urlNum / 2;
+                    listOfNums += urlNum + " ";
+                } else if (urlNum % 2 == 1) {
+                    urlNum = (urlNum * 3) + 1;
+                    listOfNums += urlNum + " ";
+                }
+
+                //System.out.println(number);
+                //return String.valueOf(number);
 
 
+            } while (urlNum != 1);
+
+            System.out.println();
+            return listOfNums;
+        }
 
 
-        } while (urlNum != 1) ;
-
-        System.out.println();
-        return listOfNums;
     }
-
-
 }
 
 
